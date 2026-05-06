aca2c9950107ad0844a4ecac303b9195cc7dfa86
jenilD-2009 <dudhelajenil@gmail.com>
2026-05-06 23:28:14 +0000

    update.pet

--- oop/OOP/1a. pet_variables.py
+++ oop/OOP/1a. pet_variables.py
@@ -15,3 +15,39 @@ account_balance = 129.95
 #4 Prompt user for updated credit card number and save new number
 #5 Change owner name to Alex Jones
 #6 Subtract $25 from account balance
+
+
+
+owner_name = "John"
+animal_category = input("What type of animal do you have?")
+age = 4
+Not_vaccinated = False
+creditcard = input("Please Enter new Credit Card Number")
+billing_address = "17 Park Street, The Shire 2695"
+owner_name = "Alex Jones"
+account_balance = 129.95 - 25
+
+def help():
+  print('Welcome to the Pet Data Management System')
+  print("Every vet's best friend")
+
+def increase_age():
+  global age
+  age = age + 1
+
+def verify_credit_card(creditcard):
+ if len(creditcard) == 19:
+if len(creditcard.split()) == 4:
+return True
+  return False
+ num = "5765 7574 7247"
+if verify_credit_card(creditcard): == True:
+print("VALID")
+else:
+print("INVALID")
+
+
+
+help()
+increase_age()
+print(age)
